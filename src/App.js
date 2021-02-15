9c67cb62e3a6d881e09fc5743bd6247a50d4ae96
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-15 18:04:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
-function App() {
+// this is the connection to the backend
+const socket = io.connect('http://localhost:8080');
+
+const App = () => {
+  const [data, setData] = useState({ message: '', username: '' });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on('message', ({ username, message }) => {
+      setChat([...chat, { username, message }]);
+    });
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    // stop page refresh
+    e.preventDefault();
+    const { username, message } = data;
+    socket.emit('message', { username, message });
+    // setData({ message: '', username });
+  };
+
+  const renderChat = () => {
+    return chat.map(({ username, message }, index) => (
+      <div key={index}>
+        <h3>
+          {username}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <form onSubmit={handleSubmit}>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <TextField
+            name="username"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={data.username}
+            label="Username"
+          />
+        </div>
+        <div>
+          <TextField
+            name="message"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={data.message}
+            label="Message"
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderChat()}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
